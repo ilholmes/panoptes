6cf6023fa4b23af5ba83fbea154c18e02324c235
iholmes <iholmes@yahoo-inc.com>
2018-11-02 15:48:51 -0700

    Add basic tests for properties.

--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -11,6 +11,11 @@ from yahoo_panoptes.framework.resources import PanoptesContext
 
 from .helpers import get_test_conf_file
 
+
+def _callback():
+    pass
+
+
 class TestPanoptesConsumer(unittest.TestCase):
     def setUp(self):
         self.my_dir, self.panoptes_test_conf_file = get_test_conf_file()
@@ -19,7 +24,19 @@ class TestPanoptesConsumer(unittest.TestCase):
     def test_basic_operations(self):
         panoptes_consumer = PanoptesConsumer(panoptes_context=self._panoptes_context,
                                              consumer_type=PanoptesConsumerTypes.METRICS,
-                                             topics=[PanoptesConsumerTypes.METRICS],
+                                             topics=['metrics'],
                                              client_id="test",
                                              group="test_group",
-                                             keys=)
+                                             keys=["test_key"],
+                                             poll_timeout=60,
+                                             callback=_callback)
+
+        # Test properties
+        self.assertEqual(panoptes_consumer.panoptes_context, self._panoptes_context)
+        self.assertEqual(panoptes_consumer.client_id, "test")
+        self.assertEqual(panoptes_consumer.group, "test_group")
+        # TODO seconds or milliseconds?
+        self.assertEqual(panoptes_consumer.poll_timeout, 60000)
+        self.assertEqual(panoptes_consumer.consumer_type, PanoptesConsumerTypes.METRICS)
+        self.assertListEqual(panoptes_consumer.keys, ["test_key"])
+        panoptes_consumer.start_consumer()
